1b901ca2da0698445d00d0e766e0b775fba532d4
Himanshu Kochlay <145068175+Himansh1997@users.noreply.github.com>
2026-07-01 08:26:35 +0530

    Add: pacific-atlantic-water-flow - submission-5

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-5.cpp
@@ -0,0 +1,43 @@
+class Solution{
+    public:
+    void DFS(int i, int j, int prevCellValue, vector<vector<bool>>& visited, vector<vector<int>>& heights){
+        if(i<0 || j<0 || i>=heights.size() || j>=heights[0].size() || visited[i][j] || heights[i][j] <prevCellValue){
+            return;
+        }
+        visited[i][j]= true;
+        vector<vector<int>>directions= {{0,1},{0,-1},{1,0},{-1,0}};
+        for(int k=0; k<directions.size(); k++){
+            int x= i + directions[k][0];
+            int y= j + directions[k][1];
+
+            DFS(x, y, heights[i][j], visited, heights);
+        }
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights){
+        int row= heights.size();
+        int col= heights[0].size();
+        vector<vector<int>> ans;
+
+        vector<vector<bool>>pacificVisited(row, vector<bool>(col, false));
+        vector<vector<bool>>atlanticVisited(row, vector<bool>(col, false));
+
+        for(int i=0; i<row; i++){
+            DFS(i, 0, INT_MIN, pacificVisited, heights);
+            DFS(i, col-1, INT_MIN, atlanticVisited, heights);
+        }
+
+        for(int j=0; j<col; j++){
+            DFS(0, j, INT_MIN, pacificVisited, heights);
+            DFS(row-1, j, INT_MIN, atlanticVisited, heights);
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(pacificVisited[i][j] && atlanticVisited[i][j]){
+                    ans.push_back({i, j});
+                }
+            }
+        }
+        return ans;
+    }
+};
