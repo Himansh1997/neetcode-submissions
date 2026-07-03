1da033164f8cf99677ef7d995c15fdcc64b663b1
Himanshu Kochlay <145068175+Himansh1997@users.noreply.github.com>
2026-07-03 13:40:26 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void DFS(int n, unordered_map<int, bool>& visited, unordered_map<int, list<int>>& adj){
+        visited[n]=true;
+        for(auto i:adj[n]){
+            if(!visited[i]){
+                DFS(i, visited, adj);
+            }
+        }
+    }
+    void BFS(int n, unordered_map<int, bool>& visited, unordered_map<int, list<int>>& adj){
+        queue<int> q;
+        q.push(n);
+        visited[n]= true;
+        while(!q.empty()){
+            int current= q.front();
+            q.pop();
+
+            for(auto i: adj[current]){
+                if(!visited[i]){
+                    visited[i]=true;
+                    q.push(i);
+                }
+            }
+        }
+    }
+    int countComponents(int n, vector<vector<int>>& edges) {
+        unordered_map<int, list<int>> adj;
+        for(int i=0; i<edges.size(); i++){
+            int u= edges[i][0];
+            int v= edges[i][1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        unordered_map<int, bool> visited;
+        int count=0;
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                BFS(i, visited, adj);
+                count++;
+            }
+        }
+        return count;
+    }
+};
